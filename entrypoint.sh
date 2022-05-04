8b00c053537fa77e652d21e61d6ceeb889634757
Paul Rock <paul@drteam.rocks>
2022-05-05 00:23:51 +0300

    Configurable timeout logic added to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# If timeout is not set then use 3600 seconds (1 hour) by default
+TS_TIMEOUT="${TS_TIMEOUT:-3600}"
+
+# Run infinity loop
 while true; do
   python /app/twitterSays.py
-  sleep 300
+  sleep $TS_TIMEOUT
 done
